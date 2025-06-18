8e903d46df717c0e2b01d6f1982f1c6ce9ab7765
Tracey Kadenyi <treykadenyi@gmail.com>
2025-06-18 18:16:15 +0300

    Add previous reports route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Toaster } from "react-hot-toast";
 
 import LandingPage from "./Pages/LandingPage";
 import ResultsPage from "./Pages/ResultsPage";
+import PreviousReports from "./Pages/PreviousReports";
 import "./App.css";
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/results-page" element={<ResultsPage />} />
+          <Route path="/previous-reports" element={<PreviousReports />} />
         </Routes>
       </Router>
     </>
