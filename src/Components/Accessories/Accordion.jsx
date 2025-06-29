deed4c66e22d0e7ec663a803ffc70bf00b5c302c
Tracey Kadenyi <treykadenyi@gmail.com>
2025-06-29 15:14:35 +0300

    Adjust accordion for dark theme

--- src/Components/Accessories/Accordion.jsx
+++ src/Components/Accessories/Accordion.jsx
@@ -15,8 +15,9 @@ const Accordion = ({ items = [], renderTitle, renderContent }) => {
         <div
           key={index}
           className={clsx("w-full rounded-2xl border transition-all", {
-            "bg-white shadow-md border-green-100": openIndex === index,
-            "bg-white shadow-sm hover:shadow-md border-gray-100":
+            "bg-white dark:bg-gradient-to-b dark:from-blue-950 dark:to-gray-900 shadow-md border-green-100 dark:border-green-900":
+              openIndex === index,
+            "bg-white dark:bg-blue-950 shadow-sm hover:shadow-md border-gray-100 dark:border-gray-800":
               openIndex !== index,
           })}
         >
@@ -25,8 +26,8 @@ const Accordion = ({ items = [], renderTitle, renderContent }) => {
             className={clsx(
               "w-full px-6 py-3 border-b rounded-t-2xl flex flex-col sm:justify-between sm:items-center sm:flex-row gap-2 focus:outline-none cursor-pointer transition-all duration-200 transform hover:scale-[1.01] hover:-translate-y-0.5 hover:shadow-lg",
               openIndex === index
-                ? "bg-gradient-to-r from-green-50 via-orange-50 to-blue-100 border-green-300 shadow-md text-blue-950 font-semibold"
-                : "bg-gradient-to-r from-blue-50 to-gray-100 border-gray-200 text-gray-800"
+                ? "bg-gradient-to-r from-green-50 via-orange-50 to-blue-100 border-green-300 shadow-md text-blue-950 font-semibold dark:from-green-950 dark:via-gray-800 dark:to-blue-950 dark:border-green-800 dark:text-green-100"
+                : "bg-gradient-to-r from-blue-50 to-gray-100 border-gray-200 text-gray-800 dark:from-gray-900 dark:to-gray-800 dark:border-gray-700 dark:text-gray-200"
             )}
           >
             {renderTitle(item, index, openIndex === index)}
@@ -46,7 +47,7 @@ const Accordion = ({ items = [], renderTitle, renderContent }) => {
                 }}
                 className="overflow-hidden"
               >
-                <div className="px-6 pb-6 pt-4 rounded-b-2xl bg-white">
+                <div className="px-6 pb-6 pt-4 rounded-b-2xl bg-white dark:bg-gradient-to-b dark:from-gray-900 dark:to-gray-950 dark:text-gray-100">
                   {renderContent(item, index)}
                 </div>
               </motion.div>
