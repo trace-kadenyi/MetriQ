3927c0f348ac1e9203466d63d0a59f6fd19ab84f
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-01 11:40:03 +0300

    Create headerform component to handle the header form

---
+++ src/Components/Forms/HeaderForm.jsx
@@ -0,0 +1,79 @@
+import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+import { fadeUp } from "../Accessories/FramerMotion";
+import { Search } from "lucide-react";
+
+import Popup from "../Accessories/Popup";
+import useUrlForm from "../../hooks/urlForm";
+
+const HeaderForm = () => {
+  const {
+    url,
+    isValidFormat,
+    hasSubmitted,
+    handleChange,
+    handleSubmit,
+    loading,
+    setLoading,
+    showPopup,
+    setShowPopup,
+    partialResults,
+    submittedUrl,
+    showLongWaitMessage,
+  } = useUrlForm();
+  const navigate = useNavigate();
+  return (
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center bg-white dark:bg-gray-800 rounded-lg shadow-sm px-2 py-1 w-full max-w-xl"
+      >
+        <input
+          type="text"
+          value={url}
+          onChange={handleChange}
+          placeholder="Enter site URL"
+          required
+          className="flex-grow px-4 py-2 text-sm text-gray-800 dark:text-gray-100 rounded-l-lg focus:outline-none"
+        />
+        {url && !hasSubmitted && (
+          <p
+            className={`text-sm px-2 hidden sm:block ${
+              isValidFormat ? "text-green-600" : "text-red-600"
+            }`}
+          >
+            {isValidFormat ? "URL format looks good" : "Invalid URL format"}
+          </p>
+        )}
+        <button
+          type="submit"
+          className="px-3 py-2 text-white bg-blue-500 rounded-md cursor-pointer hover:bg-blue-600 disabled:opacity-60 disabled:cursor-not-allowed"
+          disabled={loading}
+        >
+          {loading ? (
+            <div className="w-4 h-4 border-2 border-white border-t-transparent rounded-full animate-spin" />
+          ) : (
+            <Search className="w-4 h-4" />
+          )}
+        </button>
+      </form>
+      {/* show popup */}
+      <div className="z-1000">
+        {showPopup && (
+          <Popup
+            showPopup={showPopup}
+            setShowPopup={setShowPopup}
+            loading={loading}
+            setLoading={setLoading}
+            partialResults={partialResults}
+            submittedUrl={submittedUrl}
+            showLongWaitMessage={showLongWaitMessage}
+            navigate={navigate}
+          />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default HeaderForm;
