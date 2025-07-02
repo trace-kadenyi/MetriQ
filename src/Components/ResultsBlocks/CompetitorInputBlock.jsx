7ab1124a8396637f28451a0c0d406c3bdae3b92f
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-02 15:34:32 +0300

    Revert back to original placeholder

--- src/Components/ResultsBlocks/CompetitorInputBlock.jsx
+++ src/Components/ResultsBlocks/CompetitorInputBlock.jsx
@@ -20,7 +20,7 @@ const CompetitorInputBlock = ({
         </label>
         <input
           type="text"
-          placeholder="Enter Competitor's URL"
+          placeholder="https://competitor.com"
           value={c.url}
           onChange={(e) => handleCompetitorChange(i, "url", e.target.value)}
           disabled={loading}
