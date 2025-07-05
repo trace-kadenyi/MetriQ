d216e8aec2a102eaed4f77bead98ad8cd8d8a39a
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-05 19:32:50 +0300

    Add comments to enhance code readability

--- src/utils/formatReports.jsx
+++ src/utils/formatReports.jsx
@@ -1,13 +1,15 @@
+// format reports, mainly time presentation
 export const formatReports = (reports) => {
   return Array.isArray(reports)
     ? reports.map((report) => {
-        const formattedDate = `${new Date(
-          report.createdAt
-        ).toLocaleDateString("en-US", {
-          day: "numeric",
-          month: "long",
-          year: "numeric",
-        })} at ${new Date(report.createdAt).toLocaleTimeString("en-US", {
+        const formattedDate = `${new Date(report.createdAt).toLocaleDateString(
+          "en-US",
+          {
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+          }
+        )} at ${new Date(report.createdAt).toLocaleTimeString("en-US", {
           hour: "numeric",
           minute: "2-digit",
           hour12: true,
