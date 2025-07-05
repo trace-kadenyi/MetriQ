b0cef5ead0005fae891b61d5063358b8f80638cf
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-05 19:17:06 +0300

    import usecomparecompetitors hook

--- src/hooks/useAiComparison.jsx
+++ src/hooks/useAiComparison.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+import useCompareCompetitors from "./useCompareCompetitors";
+
 export const useAiComparison = () => {
   const [aiComparison, setAiComparison] = useState("");
 };
