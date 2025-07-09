1fcd4fa8198d64c3c74fdf412a402f8b116c94e4
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-09 15:23:59 +0300

    Remove wrap style

--- src/Components/Headers/PrimaryHeader.jsx
+++ src/Components/Headers/PrimaryHeader.jsx
@@ -27,7 +27,7 @@ const PrimaryHeader = () => {
         className={`max-w-7xl mx-auto flex justify-between gap-4 p-3 ${
           isLandingPage
             ? "flex-col sm:flex-row"
-            : "flex-wrap items-center justify-between sm:flex-nowrap"
+            : "flex-col items-center justify-between md:flex-row"
         }
         `}
       >
