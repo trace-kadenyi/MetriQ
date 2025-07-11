c6597e12d1272214cad21992433e3796bfa9bd0d
Tracey Kadenyi <treykadenyi@gmail.com>
2025-07-12 01:17:48 +0300

    Add button type

--- src/Components/Buttons/LoginButtons.jsx
+++ src/Components/Buttons/LoginButtons.jsx
@@ -78,6 +78,7 @@ export default function LoginButtons({ variant = "header" }) {
         </span>
 
         <button
+          type="button"
           onClick={handleLogout}
           className="group flex items-center gap-1 hover:text-red-500 transition cursor-pointer"
           aria-label="Logout"
@@ -94,6 +95,7 @@ export default function LoginButtons({ variant = "header" }) {
   // ───────── “Login” Pill in Header ─────────
   return (
     <button
+      type="button"
       onClick={() => navigate("/login")}
       className="w-15 cursor-pointer text-xs bg-gray-50 dark:bg-gray-200 p-1 rounded-md text-gray-800 font-semibold hover:bg-orange-400 hover:text-gray-50"
       aria-label="Sign In"
