f30c93e6084932b971b4ed3315090e2f9dd47404
Tracey Kadenyi <treykadenyi@gmail.com>
2026-04-08 10:54:53 +0300

    Change API base URL to Vercel deployment

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.metriq.traceykadenyi.com",
+  baseURL: "https://metri-q-api.vercel.app",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
