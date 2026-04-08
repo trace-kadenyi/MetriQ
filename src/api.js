992fd79edebd7e709e40701dab8ae6c8795a52e2
Tracey Kadenyi <treykadenyi@gmail.com>
2026-04-08 10:48:16 +0300

    Update API base URL to new endpoint

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://metriq.traceykadenyi.com",
+  baseURL: "https://api.metriq.traceykadenyi.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
